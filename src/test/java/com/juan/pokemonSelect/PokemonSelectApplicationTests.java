e8810220ac61b9b462b31dcae8cb0085e5086de9
jchain4 <juanglezfdez97@outlook.com>
2024-07-04 21:53:19 +0200

    first commit

---
+++ src/test/java/com/juan/pokemonSelect/PokemonSelectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.pokemonSelect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonSelectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
